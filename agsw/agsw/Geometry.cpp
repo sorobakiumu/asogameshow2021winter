875be549eb5ee3e8046f37d54ce402ef326d1db1
akath <akath@DESKTOP-0D1ST1E>
2021-02-05 15:26:59 +0900

    とりあえずUP

---
+++ agsw/agsw/Geometry.cpp
@@ -0,0 +1,75 @@
+#include"Geometry.h"
+#include<DxLib.h>
+#include<cmath>
+
+void
+Vector2::operator*=(float scale) {
+	x *= scale;
+	y *= scale;
+}
+
+Vector2
+Vector2::operator*(float scale) {
+	return Vector2(x*scale, y*scale);
+}
+
+Vector2 operator+(const Vector2& va, const Vector2 vb) {
+	return Vector2(va.x + vb.x, va.y + vb.y);
+}
+
+Vector2 operator-(const Vector2& va, const Vector2 vb){
+	return Vector2(va.x - vb.x, va.y - vb.y);
+}
+
+float
+Vector2::Magnitude()const {
+	return hypot(x, y);
+}
+
+void 
+Vector2::Normalize() {
+	float mag = Magnitude();
+	x /= mag;
+	y /= mag;
+}
+
+Vector2
+Vector2::Normalized() {
+	float mag = Magnitude();
+	return Vector2(x / mag,	y /mag);
+}
+
+///内積を返す
+float
+Dot(const Vector2& va, const Vector2& vb) {
+	return va.x*vb.x + va.y*vb.y;
+}
+
+///外積を返す
+float
+Cross(const Vector2& va, const Vector2& vb) {
+	return va.x*vb.y - vb.x*va.y;
+}
+
+///内積演算子
+float 
+operator*(const Vector2& va, const Vector2& vb) {
+	return Dot(va, vb);
+}
+
+///外積演算子
+float 
+operator%(const Vector2& va, const Vector2& vb) {
+	return Cross(va, vb);
+}
+
+void 
+Vector2::operator+=(const Vector2& v) {
+	x += v.x;
+	y += v.y;
+}
+void 
+Vector2::operator-=(const Vector2& v) {
+	x -= v.x;
+	y -= v.y;
+}
---
+++ agsw/agsw/Geometry.h
@@ -0,0 +1,40 @@
+#pragma once
+
+//2D座標・ベクトルを表す構造体
+struct Vector2 {
+	Vector2():x(0),y(0){}
+	Vector2(float inx,float iny):x(inx),y(iny){}
+	float x, y;
+	///ベクトルの大きさを返します
+	float Magnitude()const;
+	
+	///正規化(大きさを１に)します
+	void Normalize();
+
+	///正規化ベクトルを返します
+	Vector2 Normalized();
+
+	void operator+=(const Vector2& v);
+	void operator-=(const Vector2& v);
+	void operator*=(float scale);
+	Vector2 operator*(float scale);
+	Vector2 operator-() {
+		return Vector2(-x, -y);
+	}
+};
+
+Vector2 operator+(const Vector2& va, const Vector2 vb);
+Vector2 operator-(const Vector2& va, const Vector2 vb);
+
+///内積を返す
+float Dot(const Vector2& va, const Vector2& vb);
+
+///外積を返す
+float Cross(const Vector2& va, const Vector2& vb);
+
+///内積演算子
+float operator*(const Vector2& va, const Vector2& vb);
+
+///外積演算子
+float operator%(const Vector2& va, const Vector2& vb);
+
