84c5db6b51581dd2e84a87f49ddaa719891ea5c6
sorobakiumu <bakatesuto_soro@ezweb.ne.jp>
2021-02-26 19:24:09 +0900

    result

--- test/test/Result.cpp
+++ test/test/Result.cpp
@@ -11,16 +11,55 @@ void Result::Run(std::shared_ptr<BaseGame>& baseGame)
 		baseGame = std::make_shared<Title>();
 		baseGame->Init();
 	}
+	flame++;
 }
 
 void Result::Draw()
 {
-	ImgMnj::GetInstance().AddImg(L"Resource/image/tile.png", Vector2(-20, -20), 0.0);
+	ImgMnj::GetInstance().AddImg(L"Resource/image/tile.png", Vector2(0, 100), 0.0);
 	ImgMnj::GetInstance().Draw();
 	ImgMnj::GetInstance().ResetD();
-	DrawFormatString(0, 0, 0xffffff, L"result");
-	for (int n = 0; n < 4; n++) {
-		DrawFormatString(0, 20+n*20, 0xffffff, L"%d = %d個",n,okasi[n]);
+	SetDrawBlendMode(DX_BLENDMODE_ALPHA, 100);
+	DrawBox(0, 0, 800, 600, GetColor(0, 0, 0), TRUE);
+	SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+	DrawRotaGraph(400,300,0.8,0.0,flameGraph,true);
+	DrawGraph(250, 110, graph[0], true);
+
+	if (flame > 60) {
+		//DrawFormatString(200, 20, 0xffffff, L"スカイクッキー = %d個", okasi[0]);
+		DrawGraph(200, 170, graph[1], true);
+		numberDraw(okasi[0],Vector2(500, 170));
+		DrawGraph(500, 170, graph[7], true);
+	}
+	if (flame > 90) {
+		//DrawFormatString(200, 20 + 1 * 20, 0xffffff, L"コーンソード = %d個", okasi[1]);
+		DrawGraph(200, 210, graph[2], true);
+		numberDraw(okasi[1], Vector2(500, 210));
+		DrawGraph(500, 210, graph[7], true);
+	}
+	if (flame > 120) {
+		//DrawFormatString(200, 20 + 2 * 20, 0xffffff, L"うまか棒 = %d個",okasi[2]);
+		DrawGraph(200, 250, graph[3], true);
+		numberDraw(okasi[2], Vector2(500, 250));
+		DrawGraph(500, 250, graph[7], true);
+	}
+	if (flame > 150) {
+		//DrawFormatString(200, 20 + 3 * 20, 0xffffff, L"トロルチョコ = %d個", okasi[3]);
+		DrawGraph(200, 290, graph[4], true);
+		numberDraw(okasi[3], Vector2(500, 290));
+		DrawGraph(500, 290, graph[7], true);
+	}
+	if (flame > 180&&flame<300) {
+		//DrawFormatString(200, 200, 0xffffff, L"合計 %000d円分買いました", rand() % 1000);
+		DrawGraph(200, 400, graph[5], true);
+		numberDraw(rand() % 1000, Vector2(400, 400));
+		DrawGraph(400, 400, graph[6], true);
+	}
+	if (flame >= 300) {
+		//DrawFormatString(200, 200, 0xffffff, L"合計 %000d円分買いました", sum);
+		DrawGraph(200, 400, graph[5], true);
+		numberDraw(sum, Vector2(400, 400));
+		DrawGraph(400, 400, graph[6], true);
 	}
 }
 
@@ -46,10 +85,30 @@ void Result::Init()
 	}
 
 	flame = 0;
-	//okasiList_[0] = L"スカイクッキー";
-	//okasiList_[1] = L"コーンソード";
-	//okasiList_[2] = L"うまか棒";
-	//okasiList_[3] = L"トロルチョコ";
+	sum = 0;
+	for (int n = 0; n < 4; n++) {
+		sum += valueTable[n] * okasi[n];
+	}
+	flameGraph = LoadGraph(L"Resource/image/フレーム.png");
+	number[0] = LoadGraph(L"Resource/image/0.png");
+	number[1] = LoadGraph(L"Resource/image/1.png");
+	number[2] = LoadGraph(L"Resource/image/2.png");
+	number[3] = LoadGraph(L"Resource/image/3.png");
+	number[4] = LoadGraph(L"Resource/image/4.png");
+	number[5] = LoadGraph(L"Resource/image/5.png");
+	number[6] = LoadGraph(L"Resource/image/6.png");
+	number[7] = LoadGraph(L"Resource/image/7.png");
+	number[8] = LoadGraph(L"Resource/image/8.png");
+	number[9] = LoadGraph(L"Resource/image/9.png");
+
+	graph[0] = LoadGraph(L"Resource/image/結果.png");
+	graph[1] = LoadGraph(L"Resource/image/スカイクッキー.png");
+	graph[2] = LoadGraph(L"Resource/image/コーンソード.png");
+	graph[3] = LoadGraph(L"Resource/image/うまか棒.png");
+	graph[4] = LoadGraph(L"Resource/image/トロルチョコ.png");
+	graph[5] = LoadGraph(L"Resource/image/合計.png");
+	graph[6] = LoadGraph(L"Resource/image/円分買いました.png");
+	graph[7] = LoadGraph(L"Resource/image/個.png");
 
 }
 
@@ -65,4 +124,32 @@ int Result::value(int& okasivalue, int& val)
 		}
 	}
 	return num;
+}
+
+void Result::numberDraw(int num,Vector2 pos)
+{
+	auto one = num % 10;
+	DrawGraph(pos.x-30,pos.y,number[one],true);
+	num -= one;
+	if (num <= 0) {
+		return;
+	}
+	auto ten = (num/10) % 10;
+	DrawGraph(pos.x - 60, pos.y, number[ten], true);
+	num -= ten*10;
+	if (num <= 0) {
+		return;
+	}
+	auto hund = (num / 100) % 10;
+	DrawGraph(pos.x - 90, pos.y, number[hund], true);
+	num -= hund * 100;
+	if (num <= 0) {
+		return;
+	}
+	auto thand = (num / 1000) % 10;
+	DrawGraph(pos.x - 120, pos.y, number[thand], true);
+	num -= thand * 100;
+	if (num <= 0) {
+		return;
+	}
 }
