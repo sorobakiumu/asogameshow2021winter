28d525ce56cbbab6ec276941522a247fb076d99e
sorobakiumu <bakatesuto_soro@ezweb.ne.jp>
2021-02-26 15:36:59 +0900

    aa

--- test/test/Result.h
+++ test/test/Result.h
@@ -1,5 +1,10 @@
 #pragma once
 #include "BaseGame.h"
+#include"Geometry.h"
+#include <vector>
+#include <tuple>
+#include <string>
+
 class Result :
     public BaseGame
 {
@@ -7,5 +12,12 @@ public:
 	void Run(std::shared_ptr<BaseGame>& baseGame) override;
 	void Draw() override;
 	void Init() override;
+private:
+	std::vector<Vector2> button;
+	int valueTable[4];
+	int okasi[4];
+	int flame;
+	int value(int& okasivalue,int& val);
+	std::vector<std::wstring> okasiList_;
 };
 
